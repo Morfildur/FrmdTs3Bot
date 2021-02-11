40eb257f51d627fe9c24bf50f52a5c82932b19eb
Jaschar Otto <git@jaschar-otto.de>
2021-02-11 13:05:10 +0100

    snapshot

--- Worker.cs
+++ Worker.cs
@@ -62,23 +62,21 @@ namespace Ts3Bot
             List<int> frmdGroups = new List<int>();
 
             logger.LogInformation("Trying to Connect to {Host}", ts3Host);
-            using (TeamSpeakClient rc = new TeamSpeakClient(ts3Host))
-            {
-                await rc.Connect();
-                logger.LogInformation("Connected at: {Time}", DateTimeOffset.Now);
+            using TeamSpeakClient rc = new TeamSpeakClient(ts3Host);
+            await rc.Connect();
+            logger.LogInformation("Connected at: {Time}", DateTimeOffset.Now);
 
-                await rc.Login(ts3User, ts3Pass);
-                await rc.UseServer(1);
+            await rc.Login(ts3User, ts3Pass);
+            await rc.UseServer(1);
 
-                logger.LogInformation("Logged in at: {Time}", DateTimeOffset.Now);
+            logger.LogInformation("Logged in at: {Time}", DateTimeOffset.Now);
 
-                IReadOnlyList<GetServerGroupListInfo> serverGroups = await rc.GetServerGroups();
-                frmdGroups.AddRange(serverGroups
-                    .Where(g => g.Name == "FRMD")
-                    .Select(g => g.Id));
+            IReadOnlyList<GetServerGroupListInfo> serverGroups = await rc.GetServerGroups();
+            frmdGroups.AddRange(serverGroups
+                .Where(g => g.Name == "FRMD")
+                .Select(g => g.Id));
 
-                frmdGroups.ForEach(g => logger.LogInformation("Found FRMD Group with Id {FrmdGroupId}", g));
-            }
+            frmdGroups.ForEach(g => logger.LogInformation("Found FRMD Group with Id {FrmdGroupId}", g));
 
             if (frmdGroups.Count == 0)
             {
@@ -87,60 +85,51 @@ namespace Ts3Bot
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                using (TeamSpeakClient rc = new TeamSpeakClient(ts3Host))
+                IReadOnlyList<GetClientInfo> rawClientInfoList = await rc.GetClients();
+
+                List<GetClientInfo> clientInfoList = rawClientInfoList
+                    .Where(c => c.Type == ClientType.FullClient)
+                    .Where(c => !nameBlacklist.Contains(c.NickName))
+                    .Where(c => !checkedClientIds.Contains(c.Id))
+                    .ToList();
+
+                foreach (GetClientInfo clientInfo in clientInfoList)
                 {
-                    await rc.Connect();
-                    logger.LogInformation("Connected at: {Time}", DateTimeOffset.Now);
-
-                    await rc.Login(ts3User, ts3Pass);
-                    await rc.UseServer(1);
-
-                    IReadOnlyList<GetClientInfo> rawClientInfoList = await rc.GetClients();
-
-                    List<GetClientInfo> clientInfoList = rawClientInfoList
-                        .Where(c => c.Type == ClientType.FullClient)
-                        .Where(c => !nameBlacklist.Contains(c.NickName))
-                        .Where(c => !checkedClientIds.Contains(c.Id))
-                        .ToList();
-
-                    foreach (GetClientInfo clientInfo in clientInfoList)
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    logger.LogInformation("Found User: {User} {Id}", clientInfo.NickName, clientInfo.Id);
+                    GetClientDetailedInfo clientDetails = await rc.GetClientInfo(clientInfo);
+                    if (pokedClients.Contains(clientDetails.UniqueIdentifier))
                     {
-                        Thread.Sleep(TimeSpan.FromSeconds(1));
-                        logger.LogInformation("Found User: {User} {Id}", clientInfo.NickName, clientInfo.Id);
-                        GetClientDetailedInfo clientDetails = await rc.GetClientInfo(clientInfo);
-                        if (pokedClients.Contains(clientDetails.UniqueIdentifier))
+                        logger.LogInformation("Already messaged {User} {Id} {UniqueId}", clientDetails.NickName,
+                            clientInfo.Id, clientDetails.UniqueIdentifier);
+                    }
+                    else
+                    {
+                        if (clientDetails.ServerGroupIds.Any(gid => frmdGroups.Contains(gid)))
                         {
-                            logger.LogInformation("Already messaged {User} {Id} {UniqueId}", clientDetails.NickName,
-                                clientInfo.Id, clientDetails.UniqueIdentifier);
-                        }
-                        else
-                        {
-                            if (clientDetails.ServerGroupIds.Any(gid => frmdGroups.Contains(gid)))
+                            logger.LogInformation("Messaging {Client}", clientDetails.UniqueIdentifier);
+                            await rc.PokeClient(clientInfo,
+                                "Please sign up for Outfit Wars (See Discord #announcements)");
+                            pokedClients.Add(clientDetails.UniqueIdentifier);
+
+
+                            if (Directory.Exists(pokedCacheDirectory))
                             {
-                                logger.LogInformation("Messaging {Client}", clientDetails.UniqueIdentifier);
-                                await rc.PokeClient(clientInfo,
-                                    "Please sign up for Outfit Wars (See Discord #announcements)");
-                                pokedClients.Add(clientDetails.UniqueIdentifier);
-
-
-                                if (Directory.Exists(pokedCacheDirectory))
-                                {
-                                    await File.AppendAllLinesAsync(
-                                        pokedCacheFile,
-                                        new[] {clientDetails.UniqueIdentifier},
-                                        stoppingToken
-                                    );
-                                }
+                                await File.AppendAllLinesAsync(
+                                    pokedCacheFile,
+                                    new[] {clientDetails.UniqueIdentifier},
+                                    stoppingToken
+                                );
                             }
                         }
-
-
-                        checkedClientIds.Add(clientInfo.Id);
                     }
-                }
 
-                await Task.Delay(10000, stoppingToken);
+
+                    checkedClientIds.Add(clientInfo.Id);
+                }
             }
+
+            await Task.Delay(5000, stoppingToken);
         }
     }
 }
